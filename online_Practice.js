8f65d3045f08c3b70d4569cdd794cf5deb49be45
MithleshKr-Pro <coding4u2@gmail.com>
2024-08-24 18:38:50 +0530

    Reupdated

--- online_Practice.js
+++ online_Practice.js
@@ -413,10 +413,98 @@
 let h1=document.querySelector("h1");
 h1.style.color="dodgerblue"
 h1.style.fontFamily="sans-serif"
+let Row1=document.querySelectorAll(".child1");
+let Row2=document.querySelectorAll(".child2");
+let Row3=document.querySelectorAll(".child3");
+let resetBtn=document.querySelector("#reset");
+let btns=document.querySelectorAll(".btn");
+let Btn=document.getElementsByTagName("button");
+let parent=document.querySelector(".parent_div");
+let winnerMsg=document.createElement("div");
+let resetGame=document.querySelector("#reset");
+let newGame=document.querySelector(".newGame")
+// console.dir(Btn);
+// let btnText="O";
+// let gameLogic=()=>{
+//     if(btnText==="O"){
+//         btnText="X";
+//         this.innerText="X";
+//         console.log("X");
+//     }else{
+//         btnText="O";
+//         this.innerText="O";
+//         console.log("O");
+//     }
+// }
+// btns.forEach(btn=>{
+//     btn.addEventListener("click",gameLogic);
+// })
 
+let turn0=true;//playerX,//player0
+const winPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+btns.forEach((btn)=>{
+    btn.addEventListener("click",()=>{
+        console.log("Button was Clicked");
+        if(turn0){
+            btn.innerText="O";
+            turn0=false;
+        }else{
+            btn.innerText="X";
+            turn0=true;
+        }
+        btn.disabled="true";
+        checkWinner();
+    })
+})
 
+const disableBtn=()=>{
+    for (Button of btns){
+        Button.disabled="true";
+    }
+}
+const resetButn=()=>{
+    for (button of btns){
+        button.disabled=false;
+        button.innerText="";
+        winnerMsg.classList.remove("winnerMssg");
+        winnerMsg.innerText="";
+        newGame.classList.add("newGame");
 
+    }
+}
 
+const checkWinner=()=>{
+    for (pattern of winPatterns){
+        let pos1Val=btns[pattern[0]].innerText;
+        let pos2Val=btns[pattern[1]].innerText;
+        let pos3Val=btns[pattern[2]].innerText;
+        if(pos1Val!=""&& pos2Val!="" && pos3Val!=""){
+            if(pos1Val===pos2Val && pos2Val===pos3Val){
+                console.log("Winner-Winner Chicken Dinner !!","Player",pos1Val);
+                parent.prepend(winnerMsg);
+                // winnerMsg.setAttribute("class",".winnerMssg");
+                winnerMsg.classList.add("winnerMssg");
+                winnerMsg.innerText="WINNER-WINNER Player " + pos1Val + ", Chicken Dinner !!";
+                newGame.classList.add("hide");
+                newGame.classList.remove("newGame");
+                disableBtn();
+            }else{
+            }
+        }
+    } 
+}
+
+resetGame.addEventListener("click",resetButn);
+newGame.addEventListener("click",resetButn);
 
 
 // TIC TAC TOE ends
